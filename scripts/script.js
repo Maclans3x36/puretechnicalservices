306674de0392df2339724fc3d0f09ca3b2c6e6dc
maclans3x36 <callangel2k4@gmail.com>
2024-10-09 14:46:09 +0100

    second commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const flipCards = document.querySelectorAll('.flip-card');
+
+flipCards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('is-flipped');
+    });
+});
